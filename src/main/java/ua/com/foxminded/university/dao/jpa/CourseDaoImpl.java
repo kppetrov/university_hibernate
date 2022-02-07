0301e9cb1e10823df13af6852e21156684422a9e
Konstantin Petrov <k.p.petrov@gmail.com>
2022-02-07 16:34:41 +0300

    added logging and added exception catching

--- src/main/java/ua/com/foxminded/university/dao/jpa/CourseDaoImpl.java
+++ src/main/java/ua/com/foxminded/university/dao/jpa/CourseDaoImpl.java
@@ -1,12 +1,19 @@
 package ua.com.foxminded.university.dao.jpa;
 
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
 import ua.com.foxminded.university.dao.CourseDao;
+import ua.com.foxminded.university.exception.DaoException;
 import ua.com.foxminded.university.model.Course;
 
 @Repository
 public class CourseDaoImpl extends GenericDaoImpl<Course> implements CourseDao {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CourseDaoImpl.class);
 
     public CourseDaoImpl() {
         super.setEntityClass(Course.class);
@@ -14,10 +21,21 @@ public class CourseDaoImpl extends GenericDaoImpl<Course> implements CourseDao {
 
     @Override
     public Course getByIdWithDetail(int id) {
-        return  getEntityManager()
-                .createNamedQuery(Course.FIND_COURSE_BY_ID_WITH_GROUPS, Course.class)
-                .setParameter("id", id)
-                .getSingleResult();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Getting course by id with detail. id={}", id);
+        }
+        try {
+            return  getEntityManager()
+                    .createNamedQuery(Course.FIND_COURSE_BY_ID_WITH_GROUPS, Course.class)
+                    .setParameter("id", id)
+                    .getSingleResult();
+        } catch (NoResultException e) {
+            String msg = String.format("The course with id=%d does not exist", id);
+            throw new DaoException(msg, e);
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot get course by id with detail. id=%d", id);
+            throw new DaoException(msg, e);
+        }
     }
 
     @Override
--- src/main/java/ua/com/foxminded/university/dao/jpa/GenericDaoImpl.java
+++ src/main/java/ua/com/foxminded/university/dao/jpa/GenericDaoImpl.java
@@ -3,12 +3,19 @@ package ua.com.foxminded.university.dao.jpa;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ua.com.foxminded.university.dao.GenericDao;
+import ua.com.foxminded.university.exception.DaoException;
 
 public abstract class GenericDaoImpl<T> implements GenericDao<T> {
-
+    private static final Logger LOGGER = LoggerFactory.getLogger(GenericDaoImpl.class);
+    
     @PersistenceContext
     private EntityManager entityManager;
     private Class<T> entityClass;
@@ -23,31 +30,74 @@ public abstract class GenericDaoImpl<T> implements GenericDao<T> {
 
     @Override
     public List<T> getAll() {
-        return entityManager.createQuery("from " + entityClass.getName(), entityClass).getResultList();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Getting all {}", entityClass);
+        }
+        try {
+            return entityManager.createQuery("from " + entityClass.getName(), entityClass).getResultList();
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot get all %s", entityClass);
+            throw new DaoException(msg, e);
+        }
     }
 
     @Override
     public T getById(int id) {
-        return entityManager.createQuery("from " + entityClass.getName() + " where id = :id", entityClass)
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Getting {} by id. id={}", entityClass, id);
+        }
+        try {
+            return entityManager.createQuery("from " + entityClass.getName() + " where id = :id", entityClass)
                 .setParameter("id", id)
                 .getSingleResult();
+        } catch (NoResultException e) {
+            String msg = String.format("The %s with id=%d does not exist", entityClass, id);
+            throw new DaoException(msg, e);
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot get %s by id. id=%d", entityClass, id);
+            throw new DaoException(msg, e);
+        }
     }
 
     @Override
     public T insert(T item) {
-        entityManager.persist(item);
-        return item;
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Creating {}. {}", entityClass, item);
+        }
+        try {
+            entityManager.persist(item);
+            return item;
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot create %s.", item);
+            throw new DaoException(msg, e);
+        }
     }
 
     @Override
     public void update(T item) {
-        entityManager.merge(item);
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Updating {}. {}", entityClass, item);
+        }
+        try {
+            entityManager.merge(item);
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot update %s", item);
+            throw new DaoException(msg, e);
+        }
     }
 
     @Override
     public void delete(int id) {
-        entityManager.createQuery("delete from " + entityClass.getName() + " where id = :id")
-        .setParameter("id", id)
-        .executeUpdate();
+        if (LOGGER.isDebugEnabled()) {
+            LOGGER.debug("Removung {}. id={}", entityClass, id);
+        }
+        try {
+            entityManager.createQuery("delete from " + entityClass.getName() + " where id = :id")
+                .setParameter("id", id)
+                .executeUpdate();
+        } catch (PersistenceException e) {
+            String msg = String.format("Cannot remove %s by id. id=%d", entityClass, id);
+            throw new DaoException(msg, e);
+        }
     }
 }
